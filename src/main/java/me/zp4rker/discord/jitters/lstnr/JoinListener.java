1b81965f93b6f9b421ad4e5726a75bb7adad5453
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2017-09-24 22:25:13 +1000

    Added auto-role

--- src/main/java/me/zp4rker/discord/jitters/lstnr/JoinListener.java
+++ src/main/java/me/zp4rker/discord/jitters/lstnr/JoinListener.java
@@ -1,5 +1,6 @@
 package me.zp4rker.discord.jitters.lstnr;
 
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.hooks.SubscribeEvent;
 
@@ -13,6 +14,8 @@ public class JoinListener {
         event.getGuild().getTextChannelById(312571375598698507L)
                 .sendMessage("Welcome to Jitters, " + event.getUser().getAsMention() + "!" +
                         " Head on over to <#312817696578469888> and run the command `!sassign` to start adding your roles.").complete();
+        Role role = event.getGuild().getRolesByName("Member", false).get(0);
+        event.getGuild().getController().addSingleRoleToMember(event.getMember(), role).complete();
     }
 
 }
