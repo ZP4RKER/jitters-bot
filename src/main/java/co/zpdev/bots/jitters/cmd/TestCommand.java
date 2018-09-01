e7b8bf6bd445b3ea8757e19975445826ef26623f
zpdev <acc@zpdev.co>
2018-09-02 00:33:47 +1000

    final testing

--- src/main/java/co/zpdev/bots/jitters/cmd/TestCommand.java
+++ src/main/java/co/zpdev/bots/jitters/cmd/TestCommand.java
@@ -1,17 +1,8 @@
 package co.zpdev.bots.jitters.cmd;
 
-import co.zpdev.bots.jitters.Jitters;
 import co.zpdev.core.discord.command.Command;
-import co.zpdev.core.discord.exception.ExceptionHandler;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.PrivateChannel;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 /**
  * @author ZP4RKER
@@ -25,23 +16,6 @@ public class TestCommand {
     public void onCommand(Message message) {
         PrivateChannel pc = message.getAuthor().openPrivateChannel().complete();
         pc.getHistory().retrievePast(100).complete().forEach(m -> m.delete().queue());
-
-        String intro;
-        InputStream in = Jitters.class.getResourceAsStream("/intros.txt");
-        int c; StringBuilder sb = new StringBuilder();
-
-        try {
-            while ((c = in.read()) != -1) sb.append((char) c);
-
-            List<String> lines = Arrays.stream(sb.toString().split("\n")).filter(s -> !s.startsWith("//")).collect(Collectors.toList());
-            int rand = ThreadLocalRandom.current().nextInt(0, lines.size());
-            intro = lines.get(rand).replace("%user%", message.getAuthor().getAsMention());
-        } catch (IOException e) {
-            ExceptionHandler.handleException("reading file (intros.txt)", e);
-            intro =  null;
-        }
-
-        pc.sendMessage(intro).complete();
     }
 
 }
