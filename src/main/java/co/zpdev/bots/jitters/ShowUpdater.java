7956a7e5670b629477926bb9ce95ad6a2a6052d9
zpdev <acc@zpdev.co>
2018-10-10 16:23:50 +1100

    trying to find issue #5

--- src/main/java/co/zpdev/bots/jitters/ShowUpdater.java
+++ src/main/java/co/zpdev/bots/jitters/ShowUpdater.java
@@ -52,8 +52,6 @@ class ShowUpdater {
             long airTime = shows.getJSONObject(show).getJSONObject("nextepisode").getLong("airtime");
             long fiveMin = airTime - TimeUnit.MINUTES.toSeconds(5);
 
-            PostUtil.push("Time till airtime", "airtime = " + airTime + ", fiveMin = " + fiveMin);
-
             Timer timer = new Timer();
             if (fiveMin > 0) timer.schedule(new TimerTask() {
                 @Override
